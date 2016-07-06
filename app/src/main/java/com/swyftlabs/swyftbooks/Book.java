1f88d709cad07e2f544144e3078ed8568d0f01fc
gtaylor5 <taylor.gerard13@gmail.com>
2016-07-06 18:37:54 -0400

    Update Book.java
    added comments

--- app/src/main/java/com/swyftlabs/swyftbooks/Book.java
+++ app/src/main/java/com/swyftlabs/swyftbooks/Book.java
@@ -19,7 +19,8 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 public class Book implements Serializable {
-
+    
+    //standard book attributes
     String ISBN = "";
     String bookTitle = "something";
     String bookAuthor = "something";
@@ -29,10 +30,10 @@ public class Book implements Serializable {
     String condition = "";
     String cheggPID = "";
 
-
+    //unnecessary --> XML file is already in retailer class
     String BuyXMLFile = "stuff";
 
-
+    //purchase prices
     double listPrice = 0.0;
     double newPrice = 0.0;
     double usedPrice = 0.0;
@@ -40,7 +41,7 @@ public class Book implements Serializable {
     double eBookPrice = 0.0;
     double buyBackPrice = 0.0;
 
-
+    //rental prices
     double rentPrice_fall = 0.0;
     double rentPrice_spring = 0.0;
     double rentPrice_summer = 0.0;
@@ -49,29 +50,24 @@ public class Book implements Serializable {
     double rentPrice_90 = 0.0;
     double rentPrice_46 = 0.0;
 
-    double percentageSavings = 0.0;
-
-
-
+    double percentageSavings = -1.0;
+    
     Retailer retailer = new Retailer();
 
-    public Book(){
-
-
-
+    public Book(){ // default constructor
     }
 
-    public Book(String retailer){
-
+    public Book(String retailer){ //additional constructor sets retailer name
         this.retailer.retailerName = retailer;
-
     }
 
+    //set percentagesavings value
     public void setPercentageSavings(){
 
         ArrayList<Double> nonZeroPrices = new ArrayList<Double>();
         ArrayList<Double> prices = new ArrayList<Double>();
-
+        
+        //add all possible prices to prices array list and trim
         prices.add(rentPrice_46);
         prices.add(rentPrice_90);
         prices.add(rentPrice_178);
@@ -84,33 +80,30 @@ public class Book implements Serializable {
         prices.add(newPrice);
         prices.add(rentPrice_spring);
         prices.trimToSize();
+        
+        //fill non-zero prices array
         for(int j = 0; j < prices.size(); j++){
-
             if(prices.get(j) != 0){
-
                 nonZeroPrices.add(prices.get(j));
-
             }
-
         }
-
+        
+        //trim non-zeroprices array
         nonZeroPrices.trimToSize();
-
+        
+        //if there are no prices return null
         if(nonZeroPrices.size() == 0){
-
             return;
-
         }
-
+        
+        //get max and min prices
         double max = Collections.max(nonZeroPrices, null);
-        Log.i("AppInfo", String.valueOf(max));
         double min = Collections.min(nonZeroPrices, null);
-        Log.i("AppInfo", String.valueOf(min));
+        
+        //calculate percentage savings and clear arraylists
         percentageSavings = Math.abs((max-min)/max)*100;
         nonZeroPrices.clear();
         prices.clear();
-
     }
-
 }
 
