b472c44a8bceb281dd21dcf8d81f870ffd3d95d7
gtaylor5 <taylor.gerard13@gmail.com>
2016-07-06 18:40:17 -0400

    Update Retailer.java
    comments

--- app/src/main/java/com/swyftlabs/swyftbooks/Retailer.java
+++ app/src/main/java/com/swyftlabs/swyftbooks/Retailer.java
@@ -10,11 +10,12 @@ import java.io.Serializable;
 
 public class Retailer implements Serializable {
 	
-	String retailerName = "stuff";
-	String urlToSearchForBook = "stuff";
-	String XMLFile = "stuff";
-	String deepLink = "";
+	String retailerName = "stuff"; //name of seller
+	String urlToSearchForBook = "stuff"; // search url
+	String XMLFile = "stuff"; //xml file to be parsed
+	String deepLink = ""; // link to buy book on site.
 	
+	//build url based on ISBN number
 	public void buildURL(String ISBN){
 
 		switch (this.retailerName) {
@@ -23,22 +24,17 @@ public class Retailer implements Serializable {
 				this.urlToSearchForBook = "http://prices.valorebooks.com/lookup-multiple-categories?SiteID=3FZG6Y&ProductCode=" + ISBN +
 						"&TrackingID=3FZG6Y&Level=Detailed&NumberToShow=35&MinimumCondition=5&ShowEditionType=yes";
 				this.deepLink = "http://www.valorebooks.com/affiliate/buy/siteID=3FZG6Y/ISBN="+ISBN+"?default=used";
-
 				return;
-
 			case ("Chegg.com"):
 				this.urlToSearchForBook = "http://api.chegg.com/rent.svc?KEY=ada6c485ab35b1d2d8189fc08e5c9015&PW=2745708" +
 						"&R=XML&V=2.0&isbn=" + ISBN + "&with_pids=1&results_per_page=1";
 				return;
-
 			case ("Amazon.com"):
 				this.urlToSearchForBook = ""; // INSERT URL CONCATENTATION HERE
 				return;
-
 			case ("half.com"):
 				this.urlToSearchForBook = "";// INSERT URL CONCATENTATION HERE
 				return;
-
 			case ("Textbooks.com"):
 				this.urlToSearchForBook = "";// INSERT URL CONCATENTATION HERE
 				return;
@@ -52,28 +48,19 @@ public class Retailer implements Serializable {
 				return;
 			case("VitalSource.com"):
 				this.urlToSearchForBook = "/v2/product-search?website-id=8044180&advertiser-ids=2544507&isbn="+ISBN;
-
+				return;
 		}
-
 	}
-
+	
+	//chegg deeplink has special requirements
 	public void setCheggDeepLink(String pid){
-
 		this.deepLink = "http://www.chegg.com/?referrer=ada6c485ab35b1d2d8189fc08e5c9015&pids="+pid;
-
 	}
 
-	public Retailer(){
-
-
+	public Retailer(){// default constructor
 	}
 
 	public Retailer(String name){
-
 		this.retailerName = name;
-
 	}
-
-
-
 }
