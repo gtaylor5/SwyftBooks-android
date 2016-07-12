613dae186c5dbcf9dd7ddc471e1353d95e379c00
gtaylor5 <taylor.gerard13@gmail.com>
2016-07-12 10:22:43 -0400

    Create ValoreBook.java
    Creating custom classes for each retailer. Will enable better control of prices shown

---
+++ app/src/main/java/com/swyftlabs/swyftbooks/ValoreBook.java
@@ -0,0 +1,16 @@
+
+
+class ValoreBook extends Book {
+
+  //enter attributes for valorebook xml reseponse
+  
+  void parseValoreXML(String xmlFile){
+  
+    //placeholder method for valorebook xml parsing 
+    
+    //i.e this.book = getElements blah...
+  
+  }
+
+
+}
