91595c78e94115559cd3b55da81b29a238a31aa7
gtaylor5 <taylor.gerard13@gmail.com>
2016-07-14 12:18:45 -0400

    Update Book.java

--- app/src/main/java/com/swyftlabs/swyftbooks/Book.java
+++ app/src/main/java/com/swyftlabs/swyftbooks/Book.java
@@ -113,7 +113,7 @@ public class Book implements Serializable {
             
         }else{
             
-            lowestPriceType = "Buy"
+            lowestPriceType = "Buy";
             
         }
         
